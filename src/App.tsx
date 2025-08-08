224e44ae1440e49dca39ecee65505315852cb9a6
Somayeh Poolayi <somayeh.ply@gmail.com>
2025-08-08 10:27:11 -0700

    starting

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Store from "./pages/store/Store";
+
+import Layout from './components/layout/Layout';
+
+function App() {
+  return (
+
+    <Layout>
+       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+      </Routes>
+    </Layout>
+     
+     );
+}
+
+export default App;
