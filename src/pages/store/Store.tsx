7faed8b9ca55fb851324bb84e0be153937405038
Somayeh Poolayi <somayeh.ply@gmail.com>
2025-08-09 01:28:51 -0700

    product

--- src/pages/store/Store.tsx
+++ src/pages/store/Store.tsx
@@ -1,9 +1,25 @@
-
+import ProductItem from "../../components/productItem/ProductItem";
+import Container from '../../components/container/Container';
 
 function Store() {
   return (
-    <div>Store</div>
-  )
+    <div className="text-right mt-5">
+     
+      <h1>جدیدترین محصولات</h1>
+
+      <Container>
+        <div className="grid grid-cols-4 gap-4 mt-4">
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+        </div>
+        
+      </Container>
+      
+    </div>
+  );
 }
 
-export default Store
+export default Store;
