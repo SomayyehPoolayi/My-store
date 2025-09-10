d3e882ba1bed21452cef7e9c86ddf8950fe712f8
Somayeh Poolayi <somayeh.ply@gmail.com>
2025-09-10 03:34:17 -0700

    db.json is calling

--- src/pages/store/Store.tsx
+++ src/pages/store/Store.tsx
@@ -1,39 +1,32 @@
 import ProductItem from "../../components/productItem/ProductItem";
-import Container from '../../components/container/Container';
-import { Link } from 'react-router-dom';
+import Container from "../../components/container/Container";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getProducts } from "../../services/api";
+import type { Products } from "../../types/servers";
 
 function Store() {
+  const [products, setProducts] = useState<Products[]>([]);
+
+  useEffect(() => {
+    getProducts().then((result) => {
+      setProducts(result);
+    });
+  }, []);
+
   return (
     <div className="text-right mt-5">
-     
       <h1>جدیدترین محصولات</h1>
 
       <Container>
         <div className="grid grid-cols-4 gap-4 mt-4">
-          <Link to={`/product/${1}`}>
-          <ProductItem />
-          </Link>
-          
-          <Link to={`/product/${2}`}>
-          <ProductItem />
-          </Link>
-
-          <Link to={`/product/${3}`}>
-          <ProductItem />
-          </Link>
-
-          <Link to={`/product/${4}`}>
-          <ProductItem />
-          </Link>
-
-          <Link to={`/product/${5}`}>
-          <ProductItem />
-          </Link>
-         
+          {products.map((item) => (
+            <Link to={`/product/${item.id}`}>
+              <ProductItem {...item}/>
+            </Link>
+          ))}
         </div>
-        
       </Container>
-      
     </div>
   );
 }
---
+++ src/types/servers.ts
@@ -0,0 +1,17 @@
+import Products from '../pages/product/Product';
+
+
+export interface Products{
+  id: string
+  title: string
+  price: number
+  description: string
+  category: string
+  image: string
+  rating: ProductsRating
+}
+
+export interface ProductsRating {
+  rate: number
+  count: number
+}
