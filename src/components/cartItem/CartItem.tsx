10a5480f0974c5b8f67c2e5e366f990e5be7d602
Somayeh Poolayi <somayeh.ply@gmail.com>
2025-10-26 22:14:39 -0700

    basket  is finished

--- src/components/cartItem/CartItem.tsx
+++ src/components/cartItem/CartItem.tsx
@@ -1,19 +1,61 @@
+import { useEffect, useState } from "react";
 import Button from "../button/Button";
+import { getProduct } from "../../services/api";
+import type { IProduct } from "../../types/servers";
+import Product from "../../pages/product/Product";
+import { useShoppingCartContext } from "../../context/ShoppingCartContext";
+import { Link } from "react-router-dom";
 
-function CartItem() {
+interface ICartItem {
+  id: number;
+  qty: number;
+}
+
+function CartItem({ id, qty }: ICartItem) {
+  const [product, setProduct] = useState<IProductt>();
+
+  const {
+    handleIncreaseProductQty,
+    handleDecreaseProductQty,
+    handleRemoveProduct,
+  } = useShoppingCartContext();
+
+  useEffect(() => {
+    getProduct(id).then((data) => {
+      setProduct(data);
+    });
+  }, []);
+
   return (
     <div className="flex flex-row-reverse mt-4 border-b pb-2">
-      <img src="" alt="" className="rounded w-28 " />
+      
+      <Link to={`/product/${id}`}>
+        <img src={product?.image} alt="" className="rounded w-28 " />
+      </Link>
 
       <div className="mr-4">
-        <h3 className="text-right">عنوان محصول</h3>
+        <h3 className="text-right">{product?.title} </h3>
         <div className="mt-2">
-          <Button variant="danger" className="mr-2">
+          <Button
+            onClick={() => handleRemoveProduct(id)}
+            variant="danger"
+            className="mr-2"
+          >
             Remove
           </Button>
-          <Button variant="primary">+</Button>
-          <span className="mx-2">{2}</span>
-          <Button variant="primary">-</Button>
+          <Button
+            onClick={() => handleIncreaseProductQty(id)}
+            variant="primary"
+          >
+            +
+          </Button>
+          <span className="mx-2">{qty}</span>
+          <Button
+            onClick={() => handleDecreaseProductQty(id)}
+            variant="primary"
+          >
+            -
+          </Button>
         </div>
       </div>
     </div>
--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -4,15 +4,17 @@ import Button from "../../components/button/Button";
 import { useShoppingCartContext } from "../../context/ShoppingCartContext";
 
 function Cart() {
-
-const {cartItems}=useShoppingCartContext();
-
+  const { cartItems } = useShoppingCartContext();
 
   return (
     <div>
       <Container>
         <div>
-          <CartItem />
+          <div>
+            {cartItems.map((item) => (
+              <CartItem {...item} />
+            ))}
+          </div>
           <div className="bg-gray-200 rounded p-6">
             <p className="text-right">قیمت کل: 600</p>
             <p className="text-right">تخفیف:</p>
@@ -20,7 +22,7 @@ const {cartItems}=useShoppingCartContext();
           </div>
         </div>
         <Button variant="success" className="mt-2">
-            ثبت سفارش
+          ثبت سفارش
         </Button>
       </Container>
     </div>
